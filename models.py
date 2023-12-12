574913e26aba9f99562e93d9057e478bcc0fdb82
Brandie Lucano <blucano.dev@gmail.com>
2023-12-12 12:30:56 -0800

    user class and class methods complete

---
+++ models.py
@@ -0,0 +1,75 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
+db = SQLAlchemy()
+
+bcrypt = Bcrypt()
+
+def connect_db(app):
+    """Connect to database."""
+
+    app.app_context().push()
+    db.app = app
+    db.init_app(app)
+
+
+class User(db.Model):
+    """Site user"""
+
+    __tablename__ = "users"
+
+    username = db.Column(
+        db.String(20),
+        primary_key=True
+    )
+    password = db.Column(
+        db.Text,
+        nullable=False
+    )
+    email = db.Column(
+        db.String(50),
+        unique=True,
+        nullable=False
+    )
+    first_name = db.Column(
+        db.String(30),
+        nullable=False
+    )
+    last_name = db.Column(
+        db.String(30),
+        nullable=False
+    )
+
+    @classmethod
+    def register(cls, username, password, email, first_name, last_name):
+        """Register a user and hash their password
+        Return new user instance
+        """
+
+        hashed = bcrypt.generate_password_hash(password).decode('utf8')
+
+        return cls(
+            username=username,
+            password=hashed,
+            email=email,
+            first_name=first_name,
+            last_name=last_name
+        )
+
+    @classmethod
+    def authenticate(cls, username, password):
+        """Validate username and password.
+        Return user if valid, or False if not.
+        """
+        user = cls.query.filter_by(username=username).one_or_none()
+
+        if user and bcrypt.check_password_hash(user.password, password):
+            return user
+
+        else:
+            return False
+
+
+
+
+
