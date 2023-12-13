71bbbf9443973381f3a84380d4f4787427893758
Brandie Lucano <blucano.dev@gmail.com>
2023-12-12 17:02:44 -0800

    working on adding notes and showing notes on user page

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_debugtoolbar import DebugToolbarExtension
 from werkzeug.exceptions import Unauthorized
 
 from models import connect_db, db, User, Note
-from forms import RegisterForm, LoginForm, CSRFProtectForm
+from forms import RegisterForm, LoginForm, NoteForm, CSRFProtectForm
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
@@ -20,11 +20,13 @@ db.create_all()
 
 toolbar = DebugToolbarExtension(app)
 
+
 @app.get("/")
 def homepage():
     """ Shows homepage """
 
     return redirect("/register")
+
 
 @app.route("/register", methods=['GET', 'POST'])
 def register_form():
@@ -45,7 +47,7 @@ def register_form():
         last_name = form.last_name.data
 
         user = User.register(username, password, email, first_name, last_name)
-        #can add user in register function but always commit in route
+        # can add user in register function but always commit in route
         db.session.add(user)
         db.session.commit()
 
@@ -54,6 +56,7 @@ def register_form():
         return redirect(f"/users/{user.username}")
 
     return render_template("register.html", form=form)
+
 
 @app.route("/login", methods=['GET', 'POST'])
 def login_form():
@@ -79,23 +82,8 @@ def login_form():
         else:
             form.username.errors = ["Bad name/password"]
 
-
     return render_template("login.html", form=form)
 
-
-@app.get("/users/<username>")
-def show_user(username):
-    """Show info about a logged in user"""
-
-    if "username" not in session or username != session[AUTH_KEY]:
-        flash("Unauthorized access!")
-        return redirect("/login")
-
-    user = User.query.get_or_404(username)
-    notes = Note.query.get(user.notes).all()
-    form = CSRFProtectForm()
-
-    return render_template("user_info.html", user=user, form=form, notes=notes)
 
 @app.post("/logout")
 def logout():
@@ -108,4 +96,62 @@ def logout():
         return redirect("/")
     else:
         raise Unauthorized()
+
+
+@app.post("/users/<username>/delete")
+def delete_user(username):
+    """Delete user and their notes."""
+
+    form = CSRFProtectForm()
+
+    if form.validate_on_submit():
+        user = User.query.get(username)
+        notes = Note.query.get(user.notes).all()
+        db.session.delete(notes)
+        db.session.delete(user)
+        db.session.commit()
+
+        session.pop(AUTH_KEY)
+
+        return redirect("/")
+    else:
+        raise Unauthorized()
+
+
+@app.get("/users/<username>")
+def show_user(username):
+    """Show info and notes about a logged in user"""
+
+    if "username" not in session or username != session[AUTH_KEY]:
+        flash("Unauthorized access!")
+        return redirect("/login")
+
+    user = User.query.get_or_404(username)
+    user_notes = Note.query.filter_by(owner_username=username).all()
+    print("###################user notes=", user_notes)
+    form = CSRFProtectForm()
+
+    return render_template("user_info.html", user=user, form=form, notes=user_notes)
+
+
+@app.route("/users/<username>/notes/add", methods=['GET', 'POST'])
+def add_note(username):
+    """Display form to add notes. Add note to DB and redirect"""
+
+    form = NoteForm()
+    user = User.query.get(username)
+
+    if form.validate_on_submit():
+        title = form.title.data
+        content = form.content.data
+
+        new_note = Note(title=title,
+                        content=content,
+                        username=username)
+        db.session.add(new_note)
+        db.session.commit()
+
+        return redirect(f"/users/{username}")
+
+    return render_template("create_note.html", form=form, user=user)
 
